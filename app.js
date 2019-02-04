02f6ff382a135f95ed3c8a30750dd43c6e9f69ae
Kamil <kamilnorowski@gmail.com>
2019-02-04 15:01:36 +0100

    db connection, errors handlers -> before clean code

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+require("./init/error-handling")();
+require("./init/db")();
+
+app.use(helmet());
+app.use(morgan('tiny'));
+
+
 
 app.get('/', (req, res, next) => {
     res.send("XXX");
-    });
+});
 
+
+app.use(function (error, req, res, next) {
+    res.status(500).send("Błąd serwera. Z ostatniego middleware. ");
+})
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening ${port}!`));
---
+++ init/db.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+module.exports = function () {
+    const dbString = "mongodb://admin:azipera1@ds123224.mlab.com:23224/newyearnewme";
+
+    const mongodb = mongoose.connect(dbString, {useNewUrlParser: true})
+    .then(
+        () => { console.log("rdy");/** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+        // err => { console.log("err"); 
+        // throw new Error(err) /** handle initial connection error */ 
+    // }
+    )
+    .catch((err) => { 
+        console.log(err.errmsg);
+        throw new Error(err)
+    })
+
+
+    // mongoose.connect(dbString, { useNewUrlParser: true }, function (error, callback) {
+    //     if (error) {
+    //         throw new Error("Nie można połączyć się z bazą danych.");
+    //     } else {
+    //         console.log("Połączono z bazą danych.");
+    //     }
+    // });
+
+}
