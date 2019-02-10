e1646c79c64912147cabd50fd38cb61404a7006f
KamilXP <kamilnorowski@gmail.com>
2019-02-10 23:58:07 +0100

    work in progress angular basic components

--- client/newyearnewme-client/src/app/app.module.ts
+++ client/newyearnewme-client/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UsersComponent } from './components/users/users.component';
+import { RouterModule, Routes } from '@angular/router';
+
+// routes
+export const ROUTES: Routes = [
+  {
+      path: 'users',
+      pathMatch: 'full',
+      // loadChildren: './projects/projects.module#ProjectsModule'
+  },
+  { path: '', pathMatch: 'full', redirectTo: '/users' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)    
   ],
   providers: [],
   bootstrap: [AppComponent]
