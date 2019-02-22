223595c4ab2a2b541b4fb68edb7a987ec279621a
Kamil <kamilnorowski@gmail.com>
2019-02-22 08:51:22 +0100

    add header

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { UserListItemComponent } from './users/user-list-item/user-list-item.component';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { UserListItemComponent } from './users/user-list/user-list-item/user-list-item.component';
 import { UsersComponent } from './users/users.component';
+import { UsersModule } from './users/users.module';
+import { UsersResolveService } from './users/users-resolve.service';
 
-const routes: Routes = [{
-	path: 'users',
-	component: UsersComponent,
-	children: [{
-		path: ':id',
-		component: UserListItemComponent
-	}]
-}]
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () => UsersModule
+  },
+  {
+    path: '**',
+    redirectTo: 'users'
+  }
+];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      // preload all modules; optionally we could
+      // implement a custom preloading strategy for just some
+      // of the modules (PRs welcome 😉)
+      preloadingStrategy: PreloadAllModules
+    })
+  ],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
