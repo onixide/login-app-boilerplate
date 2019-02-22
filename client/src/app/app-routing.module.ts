33661b96f24579719d812b64faf6596ccbc0dad9
Kamil <kamilnorowski@gmail.com>
2019-02-22 13:08:29 +0100

    login logout register components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { UserListItemComponent } from './users/user-list/user-list-item/user-lis
 import { UsersComponent } from './users/users.component';
 import { UsersModule } from './users/users.module';
 import { UsersResolveService } from './users/users-resolve.service';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   {
@@ -11,8 +13,16 @@ const routes: Routes = [
     loadChildren: () => UsersModule
   },
   {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
     path: '**',
-    redirectTo: 'users'
+    redirectTo: 'login'
   }
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,26 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HeaderComponent } from '../app/shared/header/header.component'
+import { HeaderComponent } from '../app/shared/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersModule } from './users/users.module';
-
-
-
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    UsersModule
-  ],
-  
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, UsersModule],
+
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
