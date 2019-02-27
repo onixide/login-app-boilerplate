a02a3ba5acc9a756a356a2e5cbc5e9d0e9a3733f
Kamil <kamilnorowski@gmail.com>
2019-02-27 13:39:29 +0100

    preparing to auth system

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HeaderComponent } from '../app/shared/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+
+import { HeaderComponent } from '../app/shared/header/header.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
